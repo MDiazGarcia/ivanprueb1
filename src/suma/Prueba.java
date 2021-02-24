5e5f223cbc2a15dd0b277b4cb831e7561cf91dc6
CEINMARK <CEINMARK@DESKTOP-4GR91FV>
2021-02-24 11:48:33 +0100

    ya ta

--- src/suma/Prueba.java
+++ src/suma/Prueba.java
@@ -6,10 +6,12 @@ public class Prueba {
 	public static void main(String arg[]) {
 		
 		Scanner ent=new Scanner(System.in);
+		System.out.println("Introduce el primer numero ");
 		int num1=ent.nextInt();
+		System.out.println("Introduce el segundo numero ");
 		int num2=ent.nextInt();
 		
 		
-		
+		System.out.println("Resultado es "+(num1+num2));
 	}
 }
