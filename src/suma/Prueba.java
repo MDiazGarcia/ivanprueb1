488e6034a1e77c2f4e7d88f274a4e0e093a6923d
dgmar <dgmar@DESKTOP-OVV763L>
2021-02-24 11:41:54 +0100

    Mario

---
+++ src/suma/Prueba.java
@@ -0,0 +1,15 @@
+package suma;
+
+import java.util.Scanner;
+
+public class Prueba {
+	public static void main(String arg[]) {
+		
+		Scanner ent=new Scanner(System.in);
+		int num1=ent.nextInt();
+		int num2=ent.nextInt();
+		
+		
+		
+	}
+}
